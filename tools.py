5298b270360994d2445e593e4a4cb749df90289d
nnk465 <112316817+nnk465@users.noreply.github.com>
2025-01-26 14:48:08 +0100

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,69 +1,147 @@
-import pandas as pd
 import requests
 import time
-fournisseur = "5ndLnEYqSFiA5yUFHo6LVZ1eWc6Rhh11K5CfJNkoHEPs"
+from datetime import datetime
+
+laundering = "G2YxRa6wt1qePMwfJzdXZG62ej4qaTC7YURzuh2Lwd3t"
+ffex = "5ndLnEYqSFiA5yUFHo6LVZ1eWc6Rhh11K5CfJNkoHEPs"
+raydium_add = "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1"
+pf_add = "39azUYFWPz3VHgKCf3VChUwbpURdCHRxjWVowf5jUJjg"
+fournisseurs = [ffex, laundering]
 
 wallet_to_subscribe = []
 wallet_to_unsubscribe = []
 watching_wallet = []
 checked = []
-def get_transfer(address, only_out = False):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0',}
-    params = {'address': address, 'token': 'So11111111111111111111111111111111111111111'}
+session = requests.Session()
+session_pf = requests.Session()
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0',
+        'Origin': 'https://solscan.io',}    
+
+def get_token_data(mint):
+    params = {
+        'address': mint,
+        'page': '1',
+        'page_size': '10',
+        'exclude_amount_zero': 'false',
+    }
+
+    response = session.get('https://api-v2.solscan.io/v2/token/transfer', params=params, headers=headers)
+    print(response.status_code)
+    return response.json()["metadata"]['tokens'][mint]
+    
+def get_transfers(address, number, only_out = True):
+    transfers = []
+    params = {
+        'address': address,
+        'page_size': '100',
+        'remove_spam': 'false',
+        'exclude_amount_zero': 'false',
+        'token': 'So11111111111111111111111111111111111111111',
+    }
     if only_out:
-        params['flow']='out'
-    response = requests.get('https://api-v2.solscan.io/v2/account/transfer/export', params=params, headers=headers)
-    lines = response.text.strip().split("\n")
-    df = pd.DataFrame([line.split(",") for line in lines[1:]], columns=lines[0].split(","))
-    return df
+        params["flow"]  = "out"
+    for i in range(1, int(number/100) + 1):
+        params['page'] = i
+        response = session.get('https://api-v2.solscan.io/v2/account/transfer', params=params, headers=headers)
+        transfers += response.json()['data']
     
-import pandas as pd
+    return transfers 
+    {'block_id': 312686644, 'trans_id': 'HKTRPANMSL2mxAdXW2K5syYdCxRYcfQ7HdCA62oo5jWLVncWcmx9iaHNbargbRZKA71zDC5FxNxPBduWAjC9P9t', 'block_time': 1736348318, 'activity_type': 'ACTIVITY_SPL_TRANSFER', 'from_address': '5ndLnEYqSFiA5yUFHo6LVZ1eWc6Rhh11K5CfJNkoHEPs', 'from_token_account': '5ndLnEYqSFiA5yUFHo6LVZ1eWc6Rhh11K5CfJNkoHEPs', 'to_address': '8f7dpqAKEspG1UP78YqcJQQ66cdywim5cRP4Nb8UBBD7', 'to_token_account': '8f7dpqAKEspG1UP78YqcJQQ66cdywim5cRP4Nb8UBBD7', 'token_address': 'So11111111111111111111111111111111111111111', 'token_decimals': 9, 'amount': 10073347000, 'flow': 'out', 'value': 1990.8963010799998}
 
-def get_filtered_addresses(dataframe):
-    """
-    Récupère les adresses des lignes de la colonne "To" 
-    où la colonne "Amount" est comprise entre 20 et 30, "Flow" est "out",
-    et "Action" est "TRANSFER".
-
-    :param dataframe: DataFrame Pandas contenant les données
-    :return: Liste des adresses correspondantes
-    """
-    # Filtrer les lignes selon les conditions
-    dataframe['Amount'] = pd.to_numeric(dataframe['Amount'], errors='coerce')
-    filtered_rows = dataframe[
-        (dataframe['Amount'] * 10**-9 >= 20) &
-        (dataframe['Amount'] * 10**-9 <= 30) &
-        (dataframe['Flow'] == 'out') &
-        (dataframe['Action'] == 'TRANSFER')
-    ]
-    
-    # Récupérer les adresses dans la colonne "To"
-    addresses = list(set(filtered_rows['To'].tolist()))
-    
+def get_filtered_addresses(transfers):
+    addresses = []
+    for transfer in transfers:
+        if transfer['amount'] > 19000000000 and transfer['amount'] < 29900000000:
+            addresses.append(transfer['to_address'])    
     return addresses
     
 def check_add(address):
-    data = get_transfer(address)
-    return False if len(data) > 1 else True
+    data = get_transfers(address, 100, False)
+    i = 2 if len(data) > 1 and data[1]["from_address"] == laundering else 1 
+    return False if len(data) > i else True
 
-def maj_wallet_to_subscribe():    
+def get_raydium_swaps(mint):
+    params = {
+    'address': 'MB6vFR9eg69Zjq8FcNxEnPFNAS9oN5grWtczq6vpump',
+    'page_size': '100',
+    'exclude_amount_zero': 'false',
+    'page': 1}
+    response = s.get('https://api-v2.solscan.io/v2/token/transfer', params=params, headers=headers).json()["data"]
+    transfers = response
+    transfers.reverse()
+    addresses = []
+    for tx in transfers:
+        ady = tx['from_address']
+        if tx['from_address'] == raydium_add and tx['to_address'] != "GGztQqQ6pCPaJQnNpXBgELr5cs3WwDakRbh1iEMzjgSJ":
+            addresses.append((tx['to_address'], tx['value']/tx['amount']*10**6))
+    return addresses
+            
+def get_migration_time(mint):
+    response = session.get(
+        f'https://api-v2.solscan.io/v2/token/transfer?address={mint}&page=1&page_size=10&exclude_amount_zero=false&from={pf_add}&to={raydium_add}',
+        headers=headers,)
+    if response.status_code == 200:
+        if response.json()['data']:
+            return None
+        return response.json()['data'][0]['block_time']
+    else:
+        print(response.content)
+        
+def is_rug(mint, ts):
+    i = 1
     while True:
-        df = get_transfer(fournisseur, only_out=True)
-        print("get dataframe")
-        first_sort = get_filtered_addresses(df)
-        print("first sort done")
+        response = session.get(
+            f'https://api-v2.solscan.io/v2/token/transfer?address={mint}&page={i}&page_size=100&exclude_amount_zero=false&from=5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1&block_time[]={ts}&block_time[]={ts+1}',
+            cookies=cookies,
+            headers=headers,
+        )
+        i += 1
+        if len(response.json()['data']) == 0:
+            return False
+        for tx in response.json()['data']:
+            if tx["amount"]*10**-6 > 90000000:
+                return True
+                
+def swap(side, mint, amount, slippage, priorityFee, pool):
+    response = requests.post(url=f"https://pumpportal.fun/api/trade?api-key={apikey}", data={
+        "action": side,             # "buy" or "sell"
+        "mint": mint,      # contract address of the token you want to trade
+        "amount": amount,            # amount of SOL or tokens to trade
+        "denominatedInSol": "true" if side == 'buy' else 'false', # "true" if amount is amount of SOL, "false" if amount is number of tokens
+        "slippage": slippage,              # percent slippage allowed
+        "priorityFee": priorityFee,        # amount used to enhance transaction speed
+        "pool": pool               # exchange to trade on. "pump" or "raydium"
+    })
+     
+    data = response.json()           # Tx signature or error(s)
+    return data
+def maj_wallet_to_subscribe():
+    global wallet_to_subscribe
+    global wallet_to_unsubscribe
+    i = 2500
+    while True:
+        print(f"maj du suivi de wallet {datetime.now().strftime('%H:%M:%S')}")
+        news = []
+        rm = []
+        addresses = sum([get_transfers(address, i) for address in fournisseurs], [])
+        first_sort = list(set(get_filtered_addresses(addresses) + watching_wallet))
+        i = 200
         for address in first_sort:
             if address in checked:
                 continue
             if check_add(address):
-                wallet_to_subscribe.append(address)
-            elif address in watching_wallet:
-                watching_wallet.pop(address)
-                wallet_to_unsubscribe.append(address)
-                checked.append(address)
+                if address not in watching_wallet:
+                    news.append(address)
+                    watching_wallet.append(address)
             else:
                 checked.append(address)
-        time.sleep(3600*0.5)
+                if address in watching_wallet:
+                    watching_wallet.remove(address)
+                    rm.append(address)
+        wallet_to_subscribe += news
+        wallet_to_unsubscribe += rm
+        time.sleep(15*60)
 
 def send_message(message):
     # URL de l'API Telegram
@@ -77,3 +155,13 @@ def send_message(message):
 
     # Envoi de la requête
     response = requests.get(url, params=params)
+if __name__ == "__main__":
+    pass
+    # news = []
+    # addresses = sum([get_transfers(address, 1000) for address in fournisseurs], [])
+    # first_sort = list(set(get_filtered_addresses(addresses)))
+    # for address in first_sort:
+        # if check_add(address):
+            # if address not in watching_wallet:
+                # news.append(address)
+    # print(news)
